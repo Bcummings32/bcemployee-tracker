0679c71bcdf3b86d3fc37ba8b2191bdf8bfdc995
Ben Cummings <bncmmngs@aol.com>
2020-12-07 14:09:14 -0500

    intial commit

---
+++ db/seed.sql
@@ -0,0 +1,23 @@
+USE employeeDB;
+
+
+--department seed
+INSERT INTO department (name)
+VALUES
+("Admin"),
+("HR"),
+("Development"),
+("Marketing"),
+("Finance");
+
+--role
+INSERT INTO roles (title, salary, department_id)Values (1,)
+INSERT INTO roles (title, salary, department_id)Values (2,)
+INSERT INTO roles (title, salary, department_id)Values (3,)
+INSERT INTO roles (title, salary, department_id)Values (4,)
+
+
+
+--employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
